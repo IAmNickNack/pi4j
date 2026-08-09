c136330b279c847fdee12b768b645c48f1b514e7
iamnicknack <5727098+IAmNickNack@users.noreply.github.com>
2026-08-09 18:33:54 +0000

    add parallel provider to plugin

---
+++ plugins/pi4j-plugin-ffm/src/test/java/com/pi4j/plugin/ffm/providers/parallel/FFMParallelPortProviderTest.java
@@ -0,0 +1,67 @@
+package com.pi4j.plugin.ffm.providers.parallel;
+
+import com.pi4j.Pi4J;
+import com.pi4j.io.gpio.digital.DigitalOutputConfigBuilder;
+import com.pi4j.io.gpio.parallel.ParallelPort;
+import com.pi4j.io.gpio.parallel.ParallelPortConfigBuilder;
+import com.pi4j.io.gpio.parallel.ParallelPortDigitalOutputProvider;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Disabled("just an example for now")
+class FFMParallelPortProviderTest {
+
+    @Test
+    void test() throws Exception {
+        var context = Pi4J.newContextBuilder()
+            .add(new FFMParallelPortProvider())
+            .build();
+
+        var device = context.create(ParallelPortConfigBuilder.newInstance()
+            .id("test-gpio")
+            .initialDirection(ParallelPort.Direction.OUTPUT)
+            .bcm(0)
+            .bcm(5)
+            .bcm(19)
+            .bcm(26)
+            .build()
+        );
+
+        var virtualProvider = new ParallelPortDigitalOutputProvider(device);
+
+        var digital0 = virtualProvider.create(DigitalOutputConfigBuilder.newInstance()
+            .bcm(0)
+            .build()
+        );
+        var digital1 = virtualProvider.create(DigitalOutputConfigBuilder.newInstance()
+            .bcm(1)
+            .build()
+        );
+
+        // This is an annoyance which could be avoided if initialisation was done during construction.
+        device.initialize(context);
+
+        device.write(1);
+        Thread.sleep(500);
+        device.write(0x2);
+        Thread.sleep(500);
+        device.write(0x4);
+        Thread.sleep(500);
+        device.write(0x8);
+
+        Thread.sleep(1000);
+        device.write(0);
+
+        digital0.setState(1);
+        Thread.sleep(500);
+        digital1.setState(1);
+
+        Thread.sleep(1000);
+        device.write(0);
+
+        assertNotNull(device);
+    }
+
+}
