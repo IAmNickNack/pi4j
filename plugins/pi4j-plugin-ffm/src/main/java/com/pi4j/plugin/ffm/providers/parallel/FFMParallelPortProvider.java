47dfea370dba7f0353f710219e16d165570b1f68
iamnicknack <5727098+IAmNickNack@users.noreply.github.com>
2026-08-09 12:18:04 +0100

    provider doesn't return null

--- plugins/pi4j-plugin-ffm/src/main/java/com/pi4j/plugin/ffm/providers/parallel/FFMParallelPortProvider.java
+++ plugins/pi4j-plugin-ffm/src/main/java/com/pi4j/plugin/ffm/providers/parallel/FFMParallelPortProvider.java
@@ -1,16 +1,13 @@
 package com.pi4j.plugin.ffm.providers.parallel;
 
-import com.pi4j.exception.InitializeException;
 import com.pi4j.io.gpio.parallel.ParallelPort;
 import com.pi4j.io.gpio.parallel.ParallelPortConfig;
 import com.pi4j.io.gpio.parallel.ParallelPortProvider;
-import com.pi4j.plugin.ffm.common.gpio.PinFlag;
-import com.pi4j.plugin.ffm.common.gpio.enums.LineAttributeId;
-import com.pi4j.plugin.ffm.common.gpio.structs.LineAttribute;
-import com.pi4j.plugin.ffm.common.gpio.structs.LineConfig;
-import com.pi4j.plugin.ffm.common.gpio.structs.LineConfigAttribute;
 import com.pi4j.provider.ProviderBase;
 
+/**
+ * FFM GPIO Parallel Port Provider.
+ */
 public class FFMParallelPortProvider
     extends ProviderBase<ParallelPortProvider, ParallelPort, ParallelPortConfig>
     implements ParallelPortProvider {
@@ -22,7 +19,6 @@ public class FFMParallelPortProvider
 
     @Override
     public ParallelPort create(ParallelPortConfig config) {
-        return null;
+        return new FFMParallelPort(this, config);
     }
-
 }
