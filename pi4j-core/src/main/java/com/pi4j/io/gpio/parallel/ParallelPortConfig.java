d2f10e6b88284165f5b9873f9dbef0019739049f
iamnicknack <5727098+IAmNickNack@users.noreply.github.com>
2026-08-09 18:46:27 +0000

    Squashed commit of the following:
    commit 462e57768718600bc9db7573746fccc1ba24dc79
    Author: iamnicknack <5727098+IAmNickNack@users.noreply.github.com>
    Date:   Sun Aug 9 08:33:19 2026 +0100
    
        don't inherit from Gpio types
    
    # Conflicts:
    #	pi4j-core/src/main/java/com/pi4j/io/gpio/parallel/ParallelPortConfig.java
    #	pi4j-core/src/test/java/com/pi4j/io/gpio/parallel/ParallelPortTest.java

---
+++ pi4j-core/src/main/java/com/pi4j/io/gpio/parallel/ParallelPortConfig.java
@@ -0,0 +1,63 @@
+package com.pi4j.io.gpio.parallel;
+
+import com.pi4j.io.Bcm;
+import com.pi4j.io.IOConfig;
+import com.pi4j.io.gpio.digital.PullResistance;
+
+import java.util.Map;
+
+/**
+ * Configuration for a parallel port
+ * @param id the user-provided ID
+ * @param name the user-provided name
+ * @param description the user-provided description
+ * @param bus the bus number / GPIO chip
+ * @param bcm the BCM details / pin numbers
+ * @param onValue the value to set when the pin is set to HIGH
+ * @param pull the pull resistance setting
+ * @param debounce the debounce time in microseconds
+ * @param initialValue the initial value of the port
+ * @param shutdownValue the value applied to the port on shutdown
+ * @param initialDirection the initial direction of the port
+ */
+public record ParallelPortConfig(
+    @Override String id,
+    @Override String name,
+    @Override String description,
+    Integer bus,
+    Bcm bcm,
+    Integer onValue,
+    // input
+    PullResistance pull,
+    Long debounce,
+    // output
+    Integer initialValue,
+    Integer shutdownValue,
+    // port
+    ParallelPort.Direction initialDirection
+) implements IOConfig {
+
+    @Override
+    public int getUniqueIdentifier() {
+        return bcm.intMask();
+    }
+
+    @Override
+    public String provider() {
+        throw new UnsupportedOperationException("Unsupported operation");
+    }
+
+    @Override
+    public Map<String, String> properties() {
+        throw new UnsupportedOperationException("Unsupported operation");
+    }
+
+    @Override
+    public void validate() {
+        // this current core codebase doesn't seem to do anything here except check that the ID is non-null
+        // `@NonNull` annotations could be used to indicate this requirement, so leaving faults up to the user(?)
+        if (id == null) {
+            throw new IllegalStateException("ID cannot be null");
+        }
+    }
+}
